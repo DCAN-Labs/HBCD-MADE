c95d8b9f556efd10d8701426d70571c782bd4b23
Erik Lee <leex6144@umn.edu>
2022-07-22 16:06:57 -0500

    updates including dockerfile

--- scripts/run_compiled_MADE.sh
+++ scripts/run_compiled_MADE.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-output_dir_name=/panfs/roc/groups/8/faird/shared/data/TOTS_UMD_collab/code/cdl-eeg-processing/MADE-EEG-preprocessing-pipeline/MADE_edits_v104_for_sharing/example_HBCD_output_compiled/
-bids_dir=/panfs/roc/groups/8/faird/shared/data/HBCD_Style_UMD_EEG_Pilot/BIDS_manual/
-participant_label=sub-1
-session_label=ses-1
+output_dir_name=/panfs/roc/groups/8/faird/shared/data/TOTS_UMD_collab/code/cdl-eeg-processing/MADE-EEG-preprocessing-pipeline/MADE_edits_v111_ekg/testing_output/
+bids_dir=/home/midb-ig/shared/data_sources/leex6144/UMD0012_546802_V03_bids/
+participant_label=sub-UMD0012
+session_label=_
 file_extension=.set
-json_settings_file=/panfs/roc/groups/8/faird/shared/data/TOTS_UMD_collab/code/cdl-eeg-processing/MADE-EEG-preprocessing-pipeline/MADE_edits_v104_for_sharing/proc_settings_fast.json
+json_settings_file=/home/midb-ig/shared/repositories/leex6144/HBCD-MADE/testing_settings.json
 save_interim_result=1
 
-./run_MADE/for_testing/run_run_MADE.sh /home/midb-ig/shared/repositories/leex6144/v910/ $output_dir_name $bids_dir $participant_label $session_label $file_extension $json_settings_file $save_interim_result
+/panfs/roc/groups/8/faird/shared/data/TOTS_UMD_collab/code/cdl-eeg-processing/MADE-EEG-preprocessing-pipeline/MADE_edits_v111_ekg/run_MADE/for_testing/run_run_MADE.sh \
+/home/midb-ig/shared/repositories/leex6144/v910/ $output_dir_name $bids_dir $participant_label $session_label $file_extension $json_settings_file $save_interim_result
